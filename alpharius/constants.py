3aa80043adebd6aab76e94aeb6ec96d565edde06
Zheng Wang <zheng.wang.rice@gmail.com>
2022-01-15 22:16:44 -0800

    Update Nasdaq100 symbols

--- alpharius/constants.py
+++ alpharius/constants.py
@@ -1,17 +1,16 @@
 import pandas as pd
 
 NASDAQ100_SYMBOLS = [
-    'ATVI', 'ADBE', 'AMD', 'ALGN', 'GOOGL', 'AMZN', 'AEP', 'AMGN', 'ADI',
-    'ANSS', 'AAPL', 'AMAT', 'ASML', 'TEAM', 'ADSK', 'ADP', 'BIDU', 'BIIB', 'BKNG',
-    'AVGO', 'CDNS', 'CDW', 'CERN', 'CHTR', 'CHKP', 'CTAS', 'CSCO', 'CTSH', 'CMCSA',
-    'CPRT', 'COST', 'CRWD', 'CSX', 'DXCM', 'DOCU', 'DLTR', 'EBAY', 'EA', 'EXC',
-    'FAST', 'FISV', 'FOXA', 'GILD', 'HON', 'IDXX', 'ILMN', 'INCY', 'INTC',
-    'INTU', 'ISRG', 'JD', 'KDP', 'KLAC', 'KHC', 'LRCX', 'LULU', 'MAR', 'MRVL',
-    'MTCH', 'MELI', 'FB', 'MCHP', 'MU', 'MSFT', 'MRNA', 'MDLZ', 'MNST', 'NTES',
-    'NFLX', 'NVDA', 'NXPI', 'ORLY', 'OKTA', 'PCAR', 'PAYX', 'PYPL', 'PTON', 'PEP',
-    'PDD', 'QCOM', 'REGN', 'ROST', 'SGEN', 'SIRI', 'SWKS', 'SPLK', 'SBUX', 'SNPS',
-    'TMUS', 'TSLA', 'TXN', 'TCOM', 'VRSN', 'VRSK', 'VRTX', 'WBA', 'WDAY', 'XEL',
-    'XLNX', 'ZM'
+    'ATVI', 'ADBE', 'AMD', 'ABNB', 'ALGN', 'GOOGL', 'AMZN', 'AEP', 'AMGN',
+    'ADI', 'ANSS', 'AAPL', 'AMAT', 'ASML', 'TEAM', 'ADSK', 'ADP', 'BIDU', 'BIIB',
+    'BKNG', 'AVGO', 'CDNS', 'CHTR', 'CTAS', 'CSCO', 'CTSH', 'CMCSA', 'CPRT', 'COST',
+    'CRWD', 'CSX', 'DDOG', 'DXCM', 'DOCU', 'DLTR', 'EBAY', 'EA', 'EXC', 'FAST',
+    'FISV', 'FTNT', 'GILD', 'HON', 'IDXX', 'ILMN', 'INTC', 'INTU', 'ISRG', 'JD',
+    'KDP', 'KLAC', 'KHC', 'LRCX', 'LCID', 'LULU', 'MAR', 'MRVL', 'MTCH', 'MELI',
+    'FB', 'MCHP', 'MU', 'MSFT', 'MRNA', 'MDLZ', 'MNST', 'NTES', 'NFLX', 'NVDA',
+    'NXPI', 'ORLY', 'OKTA', 'PCAR', 'PANW', 'PAYX', 'PYPL', 'PTON', 'PEP', 'PDD',
+    'QCOM', 'REGN', 'ROST', 'SGEN', 'SIRI', 'SWKS', 'SPLK', 'SBUX', 'SNPS', 'TMUS',
+    'TSLA', 'TXN', 'VRSN', 'VRSK', 'VRTX', 'WBA', 'WDAY', 'XEL', 'XLNX', 'ZM', 'ZS'
 ]
 
 
