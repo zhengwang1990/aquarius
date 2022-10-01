e3941aec3c590c897819038fb4c9125a1b8ff240
Zheng Wang <zheng.wang.rice@gmail.com>
2022-10-01 11:51:11 -0700

    Improve constants coverage

--- tests/trade/test_constants.py
+++ tests/trade/test_constants.py
@@ -1,16 +1,16 @@
 import alpharius.trade as trade
-import datetime
+import pandas as pd
 import unittest
 
 
 class TestConstants(unittest.TestCase):
 
     def test_get_nasdaq100(self):
-        nasdaq100_symbols = trade.get_nasdaq100(datetime.datetime.now())
+        nasdaq100_symbols = trade.get_nasdaq100(pd.to_datetime('2015-01-01'))
         self.assertIn('AAPL', nasdaq100_symbols)
 
     def test_get_sp500(self):
-        sp500_symbols = trade.get_sp500(datetime.datetime.now())
+        sp500_symbols = trade.get_sp500(pd.to_datetime('2015-01-01'))
         self.assertIn('AAPL', sp500_symbols)
 
 
