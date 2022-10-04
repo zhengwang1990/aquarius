2b4e74a4925ad40b6d20b1d2c9d7697263288a22
Zheng Wang <zheng.wang.rice@gmail.com>
2022-10-03 18:38:39 -0700

    rm envs

--- bin/envs.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-export APCA_API_KEY_ID=""
-export APCA_API_SECRET_KEY=""
-export APCA_API_BASE_URL=""
-export EMAIL_USERNAME=""
-export EMAIL_PASSWORD=""
-export EMAIL_RECEIVER=""
-export CASH_RESERVE=25000
-export MPLBACKEND=""
