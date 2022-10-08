d42d5caf4666824dd35a3f5a617c324d54af7876
Zheng Wang <zheng.wang.rice@gmail.com>
2022-10-08 13:56:10 -0700

    Productionize web

--- alpharius/web/__init__.py
+++ alpharius/web/__init__.py
@@ -1,10 +1,13 @@
+import os
+
 from flask import Flask
 from . import dashboard
 
 
 def create_app(test_config=None):
     app = Flask(__name__, instance_relative_config=True)
-    app.config.from_mapping(SECRET_KEY='dev')
+    secret_key = os.environ.get('SECRET_KEY', 'dev')
+    app.config.from_mapping(SECRET_KEY=secret_key)
 
     if test_config:
         app.config.from_mapping(test_config)
