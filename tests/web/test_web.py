913d819aa726b8568a2197444417d5c70911ca6b
Zheng Wang <zheng.wang.rice@gmail.com>
2022-10-09 11:13:50 -0700

    Add transactions

--- tests/web/test_web.py
+++ tests/web/test_web.py
@@ -3,3 +3,8 @@ def test_dashboard(client, mock_alpaca):
     assert mock_alpaca.get_portfolio_history_call_count > 0
     assert mock_alpaca.list_orders_call_count > 0
     assert mock_alpaca.list_positions_call_count > 0
+
+
+def test_transactions(client, mock_alpaca):
+    assert client.get('/transactions').status_code == 200
+    assert mock_alpaca.list_orders_call_count > 0
