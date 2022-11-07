81014428b417290c2bfeafe10d2775df5789adb0
Zheng Wang <zheng.wang.rice@gmail.com>
2022-11-06 21:32:12 -0800

    Keep checking job status to mitigate restart impact

--- alpharius/web/scheduler.py
+++ alpharius/web/scheduler.py
@@ -36,7 +36,7 @@ def get_job_status():
 
 
 @scheduler.task('cron', id='trade', day_of_week='mon-fri',
-                hour='9-15', minute='0,30', timezone='America/New_York')
+                hour='9-15', minute='*', timezone='America/New_York')
 def trade():
     if job_status != 'running':
         _trade_impl()
