6c043a1e7ac0d3b92d450949dca3551cb558de75
Zheng Wang <zheng.wang.rice@gmail.com>
2022-11-10 22:12:01 -0800

    Refactor dashboard graph

--- alpharius/web/static/js/dashboard.js
+++ alpharius/web/static/js/dashboard.js
@@ -1,13 +1,17 @@
-let current_timeframe = sessionStorage.getItem("timeframe");
-let current_compares = sessionStorage.getItem("compares");
-if (!current_timeframe) current_timeframe = "1d";
+var active_timeframe = sessionStorage.getItem("timeframe");
+if (!active_timeframe) active_timeframe = "1d";
+var saved_compares = sessionStorage.getItem("compares");
+var active_compares = new Set();
+if (saved_compares !== null) {
+    for (const symbol of JSON.parse(saved_compares)) {
+        active_compares.add(symbol);
+    }
+}
+
 const graph_timeframes = ["1d", "1w", "1m", "6m", "1y", "5y"];
 const compare_symbols = ["qqq", "spy", "tqqq"];
 document.getElementById("current-equity").innerHTML = HISTORIES["current_equity"];
 
-var active_timeframe = null;
-var active_compares = new Set();
-var charts = {};
 const graph_point_radius = {
     "1d": 0, "1w": 3, "1m": 2,
     "6m": 0, "1y": 0, "5y": 0,
@@ -22,105 +26,78 @@ const symbol_colors = {
 };
 
 function change_timeframe(timeframe) {
-    if (active_timeframe === timeframe) {
-        return;
-    }
-    if (active_timeframe) {
-        document.getElementById("graph-" + active_timeframe).style.display = "none";
-        document.getElementById("btn-" + active_timeframe).classList.remove("active");
-    }
-    document.getElementById("graph-" + timeframe).style.removeProperty("display");
-    document.getElementById("current-change").innerHTML = HISTORIES["change_" + timeframe];
+    document.getElementById("btn-" + active_timeframe).classList.remove("active");
     document.getElementById("btn-" + timeframe).classList.add("active");
     active_timeframe = timeframe;
     sessionStorage.setItem("timeframe", active_timeframe);
 }
+document.getElementById("btn-" + active_timeframe).classList.add("active");
 
 function change_compare(symbol) {
     document.getElementById("btn-" + symbol).classList.toggle("active");
     if (active_compares.has(symbol)) {
         active_compares.delete(symbol);
-        for (const timeframe of graph_timeframes) {
-            for (var i = 0; i < charts[timeframe].data.datasets.length; i++) {
-                if (charts[timeframe].data.datasets[i].label === symbol.toUpperCase()) {
-                    charts[timeframe].data.datasets.splice(i, 1);
-                    break;
-                }
-            }
-            charts[timeframe].update();
-        }
     } else {
         active_compares.add(symbol);
-        for (const timeframe of graph_timeframes) {
-            dataset = {
-                label: symbol.toUpperCase(),
-                backgroundColor: symbol_colors[symbol],
-                borderColor: symbol_colors[symbol],
-                borderWidth: 2,
-                radius: graph_point_radius[timeframe],
-                data: HISTORIES[symbol + "_" + timeframe],
-            };
-            if (timeframe === "1d") {
-                dataset.segment = {
-                    borderColor:
-                     ctx => HISTORIES["time_1d"][ctx.p1DataIndex] > HISTORIES['market_close'] ? symbol_colors[symbol + "light"] : undefined,
-                };
-            }
-            charts[timeframe].data.datasets.push(dataset);
-            charts[timeframe].update();
-        }
     }
     sessionStorage.setItem("compares", JSON.stringify(Array.from(active_compares)));
 }
-
-change_timeframe(current_timeframe);
-for (const timeframe of graph_timeframes) {
-    document.getElementById("btn-" + timeframe).addEventListener("click", () => {
-        change_timeframe(timeframe);
-    });
-}
-for (const symbol of compare_symbols) {
-     document.getElementById("btn-" + symbol).addEventListener("click", () => {
-        change_compare(symbol);
-    });
+for (const symbol of active_compares.values()) {
+    document.getElementById("btn-" + symbol).classList.add("active");
 }
 
 var graph_data = {}
 for (const timeframe of graph_timeframes) {
-    graph_data[timeframe] = {
-        labels: HISTORIES["time_" + timeframe],
-        datasets: [
-            {
-                label: "value",
-                backgroundColor: HISTORIES["color_" + timeframe],
-                borderColor: HISTORIES["color_" + timeframe],
-                borderWidth: 2,
-                radius: graph_point_radius[timeframe],
-                data: HISTORIES["equity_" + timeframe],
-            },
-        ],
-    };
+    graph_data[timeframe] = [{
+        label: "value",
+        backgroundColor: HISTORIES["color_" + timeframe],
+        borderColor: HISTORIES["color_" + timeframe],
+        borderWidth: 2,
+        radius: graph_point_radius[timeframe],
+        data: HISTORIES["equity_" + timeframe]
+    }];
 }
-
 const light_color = HISTORIES["color_1d"] === "red" ? "rgba(252, 0, 0, 0.25)" : "rgba(0, 128, 0, 0.25)";
-graph_data["1d"].datasets[0].segment = {
-    borderColor: ctx => HISTORIES["time_1d"][ctx.p1DataIndex] > HISTORIES['market_close'] ? light_color : undefined,
+graph_data["1d"][0].segment = {
+    borderColor: ctx => HISTORIES["time_1d"][ctx.p1DataIndex] > HISTORIES['market_close'] ? light_color : undefined
 };
-graph_data["1d"].datasets.push({
+graph_data["1d"].push({
     label: "previous close",
     borderColor: 'grey',
     borderDash: [6, 6],
     borderWidth: 1,
     radius: 0,
-    data: Array(HISTORIES["time_1d"].length).fill(HISTORIES["prev_close"]),
+    data: Array(HISTORIES["time_1d"].length).fill(HISTORIES["prev_close"])
 });
+var symbol_data = {}
+for (const timeframe of graph_timeframes) {
+    symbol_data[timeframe] = {};
+    for (const symbol of compare_symbols) {
+        dataset = {
+            label: symbol.toUpperCase(),
+            backgroundColor: symbol_colors[symbol],
+            borderColor: symbol_colors[symbol],
+            borderWidth: 2,
+            radius: graph_point_radius[timeframe],
+            data: HISTORIES[symbol + "_" + timeframe]
+        };
+        if (timeframe === "1d") {
+            dataset.segment = {
+                borderColor:
+                 ctx => HISTORIES["time_1d"][ctx.p1DataIndex] > HISTORIES['market_close'] ? symbol_colors[symbol + "light"] : undefined
+            };
+        }
+        symbol_data[timeframe][symbol] = dataset;
+    }
+}
 
-const config_base = {
+const config = {
     type: "line",
+    data: {},
     options: {
         maintainAspectRatio: false,
         interaction: {
-            intersect: false,
+            intersect: false
         },
         plugins: {
             legend: {
@@ -132,24 +109,40 @@ const config_base = {
                 ticks: {
                     autoSkip: true,
                     autoSkipPadding: 15,
-                    maxRotation: 0,
+                    maxRotation: 0
                 }
             }
         },
-        spanGaps: true,
+        spanGaps: true
     }
 };
+const chart = new Chart(document.getElementById("portfolio-graph"), config);
+function update_chart() {
+    chart.data = {
+        labels: HISTORIES["time_" + active_timeframe],
+        datasets: graph_data[active_timeframe].slice()
+    }
+    for (const symbol of active_compares.values()) {
+        chart.data.datasets.push(symbol_data[active_timeframe][symbol]);
+    }
+    chart.update();
+}
+update_chart();
 
 for (const timeframe of graph_timeframes) {
-    var config = {data: graph_data[timeframe]};
-    Object.assign(config, config_base);
-    charts[timeframe] = new Chart(document.getElementById("graph-" + timeframe), config);
+    document.getElementById("btn-" + timeframe).addEventListener("click", () => {
+        if (active_timeframe === timeframe) {
+            return;
+        }
+        change_timeframe(timeframe);
+        update_chart();
+    });
 }
-
-if (current_compares !== null) {
-    for (const symbol of JSON.parse(current_compares)) {
+for (const symbol of compare_symbols) {
+     document.getElementById("btn-" + symbol).addEventListener("click", () => {
         change_compare(symbol);
-    }
+        update_chart();
+    });
 }
 
 const buttons = document.getElementsByClassName("my-btn-outline");
