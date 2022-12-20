cc68c9dc43b9f063179365c586aa05395df6cdbd
Zheng Wang <zheng.wang.rice@gmail.com>
2022-12-20 14:35:44 -0800

    Add email main func test

--- tests/notification/test_email_sender.py
+++ tests/notification/test_email_sender.py
@@ -1,3 +1,6 @@
+import argparse
+
+import pytest
 from alpharius.notification import email_sender
 
 
@@ -11,5 +14,17 @@ def test_send_summary(mock_smtp):
 def test_send_alert(mock_smtp):
     client = email_sender.EmailSender()
     client.send_alert('error_message')
+
+    mock_smtp.assert_called_once()
+
+
+@pytest.mark.parametrize('mode',
+                         ['summary', 'alert'])
+def test_main(mode, mocker, mock_smtp):
+    mocker.patch.object(
+        argparse.ArgumentParser, 'parse_args',
+        return_value=argparse.Namespace(mode=mode, error_message='fake message'))
+
+    email_sender.main()
 
     mock_smtp.assert_called_once()
