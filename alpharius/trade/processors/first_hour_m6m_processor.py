131d81ec6e151f3e52cf1834ddfb29d154cfd09c
Zheng Wang <zheng.wang.rice@gmail.com>
2023-06-25 23:27:20 -0700

    Short for first hour m6m

--- alpharius/trade/processors/first_hour_m6m_processor.py
+++ alpharius/trade/processors/first_hour_m6m_processor.py
@@ -3,8 +3,9 @@ from typing import List, Optional
 
 import numpy as np
 from ..common import (
-    ActionType, Context, DataSource, Processor, ProcessorFactory, TradingFrequency, Position,
+    ActionType, Context, DataSource, Processor, ProcessorFactory, TradingFrequency,
     ProcessorAction, DATETIME_TYPE, DAYS_IN_A_MONTH, DAYS_IN_A_QUARTER)
+from ..data_loader import get_shortable_symbols
 from ..stock_universe import IntradayVolatilityStockUniverse
 
 NUM_UNIVERSE_SYMBOLS = 20
@@ -23,6 +24,7 @@ class FirstHourM6mProcessor(Processor):
                                                                lookback_end_date,
                                                                data_source,
                                                                num_stocks=NUM_UNIVERSE_SYMBOLS)
+        self._shortable_symbols = set(get_shortable_symbols())
 
     def get_trading_frequency(self) -> TradingFrequency:
         return TradingFrequency.FIVE_MIN
@@ -35,12 +37,17 @@ class FirstHourM6mProcessor(Processor):
         if self.is_active(context.symbol):
             return self._close_position(context)
         elif context.symbol not in self._positions:
-            return self._open_position(context)
+            t = context.current_time.time()
+            if t != datetime.time(10, 0):
+                return
+            action = self._open_long_position(context)
+            if action:
+                return action
+            action = self._open_short_position(context)
+            if action:
+                return action
 
-    def _open_position(self, context: Context) -> Optional[ProcessorAction]:
-        t = context.current_time.time()
-        if t != datetime.time(10, 0):
-            return
+    def _open_long_position(self, context: Context) -> Optional[ProcessorAction]:
         market_open_index = context.market_open_index
         intraday_highs = context.intraday_lookback['High'][market_open_index:]
         intraday_closes = context.intraday_lookback['Close'][market_open_index:]
@@ -63,19 +70,67 @@ class FirstHourM6mProcessor(Processor):
         for i in range(len(intraday_highs) - 1):
             if intraday_highs[i] > intraday_highs[i + 1] and intraday_closes[i] > intraday_closes[i + 1]:
                 return
-        self._positions[context.symbol] = {'status': 'pending', 'entry_time': context.current_time}
+        self._positions[context.symbol] = {'status': 'pending',
+                                           'entry_time': context.current_time,
+                                           'side': 'long'}
         self._logger.debug(f'[{context.current_time.strftime("%F %H:%M")}] [{context.symbol}] '
                            f'Current gain: {current_gain * 100:.2f}%. L2h: {context.l2h_avg * 100:.2f}%. '
-                           f'Current price {context.current_price}.')
+                           f'Side: long. Current price {context.current_price}.')
         return ProcessorAction(context.symbol, ActionType.BUY_TO_OPEN, 1)
+
+    def _open_short_position(self, context: Context) -> Optional[ProcessorAction]:
+        if context.symbol not in self._shortable_symbols:
+            return
+        t = context.current_time.time()
+        if t != datetime.time(10, 0):
+            return
+        market_open_index = context.market_open_index
+        intraday_highs = context.intraday_lookback['High'][market_open_index:]
+        intraday_lows = context.intraday_lookback['Low'][market_open_index:]
+        intraday_closes = context.intraday_lookback['Close'][market_open_index:]
+        intraday_opens = context.intraday_lookback['Open'][market_open_index:]
+        interday_closes = context.interday_lookback['Close']
+        if len(interday_closes) < DAYS_IN_A_QUARTER:
+            return
+        if interday_closes[-1] > 0.8 * interday_closes[-DAYS_IN_A_MONTH]:
+            return
+        if interday_closes[-DAYS_IN_A_MONTH] > 0.95 * interday_closes[-DAYS_IN_A_QUARTER]:
+            return
+        if intraday_closes[0] >= context.prev_day_close:
+            return
+        if len(intraday_lows) != 6:
+            return
+        current_loss = context.current_price / context.prev_day_close - 1
+        if current_loss < 0.5 * context.h2l_avg:
+            return
+        if np.max(intraday_highs) < context.prev_day_close:
+            return
+        max_up, min_down = 0, 0
+        for i in range(len(intraday_lows) - 1):
+            if intraday_lows[i] < intraday_lows[i + 1] and intraday_closes[i] < intraday_closes[i + 1]:
+                return
+            max_up = max(max_up, intraday_closes[i] - intraday_opens[i])
+            min_down = min(min_down, intraday_closes[i] - intraday_opens[i])
+        if max_up > -min_down:
+            return
+        self._positions[context.symbol] = {'status': 'pending',
+                                           'entry_time': context.current_time,
+                                           'side': 'short'}
+        self._logger.debug(f'[{context.current_time.strftime("%F %H:%M")}] [{context.symbol}] '
+                           f'Current loss: {current_loss * 100:.2f}%. H2l: {context.h2l_avg * 100:.2f}%. '
+                           f'Side: short. Current price {context.current_price}.')
+        return ProcessorAction(context.symbol, ActionType.SELL_TO_OPEN, 1)
 
     def _close_position(self, context: Context) -> Optional[ProcessorAction]:
         position = self._positions[context.symbol]
-        if context.current_time >= position['entry_time'] + datetime.timedelta(minutes=20):
+        side = position['side']
+        wait_minutes = 20 if side == 'long' else 10
+        if context.current_time >= position['entry_time'] + datetime.timedelta(minutes=wait_minutes):
             self._logger.debug(f'[{context.current_time.strftime("%F %H:%M")}] [{context.symbol}] '
                                f'Closing position. Current price {context.current_price}.')
             self._positions.pop(context.symbol)
-            return ProcessorAction(context.symbol, ActionType.SELL_TO_CLOSE, 1)
+            action_type = ActionType.SELL_TO_CLOSE if side == 'long' else ActionType.BUY_TO_CLOSE
+            return ProcessorAction(context.symbol, action_type, 1)
 
 
 class FirstHourM6mProcessorFactory(ProcessorFactory):
