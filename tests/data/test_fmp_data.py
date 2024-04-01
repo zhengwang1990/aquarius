6d8b8a5f7ac63076c8d9a1821dc714f2de98c32f
Zheng Wang <zheng.wang.rice@gmail.com>
2024-03-31 22:18:39 -0400

    Add test for fmp data

---
+++ tests/data/test_fmp_data.py
@@ -0,0 +1,95 @@
+import os
+import json
+
+import pandas as pd
+import pytest
+import requests
+
+import alpharius.data as data
+
+
+def fake_get(url, params, *args, **kwargs):
+    comps = url.split('//')[-1].split('/')
+    if 'historical-chart' in url:
+        content = [
+            {
+                'date': '2024-03-26 16:00:00',
+                'open': 145.92,
+                'low': 145.72,
+                'high': 146,
+                'close': 145.79,
+                'volume': 1492644
+            }
+        ]
+    elif 'historical-price-full' in url:
+        symbol = comps[4]
+        content = {
+            'symbol': symbol,
+            'historical': [
+                {
+                    'date': '2024-03-26',
+                    'open': 173.8,
+                    'high': 176.61,
+                    'low': 173.18,
+                    'close': 176.53,
+                    'adjClose': 176.53,
+                    'volume': 21712747,
+                    'unadjustedVolume': 21712747,
+                    'change': 2.73,
+                    'changePercent': 1.57077,
+                    'vwap': 175.44,
+                    'label': 'October 06, 23',
+                    'changeOverTime': 0.0157077
+                },
+            ],
+        }
+    elif 'quote-short' in url:
+        symbol = comps[4]
+        content = [
+            {
+                'symbol': symbol,
+                'price': 145.85,
+                'volume': 42822124
+            }
+        ]
+    else:
+        raise ValueError('url not recognized')
+    response = requests.Response()
+    response._content = json.dumps(content).encode()
+    response.status_code = 200
+    return response
+
+
+@pytest.fixture(autouse=True)
+def mock_requests(mocker):
+    mocker.patch.object(requests, 'get', side_effect=fake_get)
+
+
+@pytest.fixture(autouse=True)
+def mock_api_key(mocker):
+    mocker.patch.dict(os.environ, {'FMP_API_KEY': 'fake_api_key'})
+
+
+@pytest.mark.parametrize('time_interval',
+                         [data.TimeInterval.FIVE_MIN,
+                          data.TimeInterval.HOUR,
+                          data.TimeInterval.DAY])
+def test_get_data(time_interval):
+    client = data.FmpData()
+    d = client.get_data('AAPL',
+                        start_time=pd.Timestamp('2024-03-26'),
+                        end_time=pd.Timestamp('2024-03-27'),
+                        time_interval=time_interval)
+    assert len(d) > 0
+
+
+def test_get_daily():
+    client = data.FmpData()
+    d = client.get_daily('AAPL', pd.Timestamp('2024-03-26'), data.TimeInterval.FIVE_MIN)
+    assert len(d) > 0
+
+
+def test_get_last_trades():
+    client = data.FmpData()
+    prices = client.get_last_trades(['AAPL', 'MSFT'])
+    assert len(prices) == 2
